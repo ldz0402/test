ff8e07b3acd5bc9d3f455c0014e2c78c0fb75b18
ldz0402 <lidongzhuo0402@gmail.com>
2022-05-21 20:49:14 +0800

    feat(main): ✨ 测试指针++和指针+1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Hello,world!"<<endl;
+    //cout<<"Hello,world!"<<endl;
+    int a=10;
+    int *p=&a;
+    printf("int* p:%d\n",p);
+    printf("int* p+1:%d\n",p+1);
+    printf("int* p+2:%d\n",p+2);
+    p++;
+    printf("int* p++:%d\n",p);
+    char ch ='a';
+    char *q=&ch;
+    printf("char* q:%d\n",q);
+    printf("char* q+1:%d\n",q+1);
+    printf("char* q+2:%d\n",q+2);
+    q++;
+    printf("char* q++:%d\n",q);
+
+    //system("pause");
     return 0;
 }
